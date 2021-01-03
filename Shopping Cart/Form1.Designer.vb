3fbcb0ad3dc5349caf5119429ee012ed493102e8
tysmith19 <76920650+tysmith19@users.noreply.github.com>
2021-01-03 13:50:08 -0800

    Add files via upload

---
+++ Shopping Cart/Form1.Designer.vb
@@ -0,0 +1,172 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lstShelf = New System.Windows.Forms.ListBox()
+        Me.lstCart = New System.Windows.Forms.ListBox()
+        Me.btnRemove = New System.Windows.Forms.Button()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.lblTotals = New System.Windows.Forms.Label()
+        Me.btnBuy = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtCustomerID = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'lstShelf
+        '
+        Me.lstShelf.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.lstShelf.FormattingEnabled = True
+        Me.lstShelf.Location = New System.Drawing.Point(12, 25)
+        Me.lstShelf.Name = "lstShelf"
+        Me.lstShelf.Size = New System.Drawing.Size(145, 134)
+        Me.lstShelf.TabIndex = 0
+        Me.lstShelf.TabStop = False
+        '
+        'lstCart
+        '
+        Me.lstCart.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.lstCart.FormattingEnabled = True
+        Me.lstCart.Location = New System.Drawing.Point(238, 25)
+        Me.lstCart.Name = "lstCart"
+        Me.lstCart.Size = New System.Drawing.Size(141, 134)
+        Me.lstCart.TabIndex = 1
+        Me.lstCart.TabStop = False
+        '
+        'btnRemove
+        '
+        Me.btnRemove.Location = New System.Drawing.Point(161, 90)
+        Me.btnRemove.Name = "btnRemove"
+        Me.btnRemove.Size = New System.Drawing.Size(75, 23)
+        Me.btnRemove.TabIndex = 1
+        Me.btnRemove.Text = "<<&Remove"
+        Me.btnRemove.UseVisualStyleBackColor = True
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(161, 61)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(75, 23)
+        Me.btnAdd.TabIndex = 0
+        Me.btnAdd.Text = "&Add>>"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'lblTotals
+        '
+        Me.lblTotals.Location = New System.Drawing.Point(382, 25)
+        Me.lblTotals.Name = "lblTotals"
+        Me.lblTotals.Size = New System.Drawing.Size(173, 71)
+        Me.lblTotals.TabIndex = 4
+        '
+        'btnBuy
+        '
+        Me.btnBuy.Location = New System.Drawing.Point(473, 122)
+        Me.btnBuy.Name = "btnBuy"
+        Me.btnBuy.Size = New System.Drawing.Size(40, 37)
+        Me.btnBuy.TabIndex = 3
+        Me.btnBuy.Text = "&Buy"
+        Me.btnBuy.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(519, 122)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(40, 37)
+        Me.btnExit.TabIndex = 6
+        Me.btnExit.TabStop = False
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(235, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(29, 15)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Cart"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(13, 6)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(38, 15)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "DVDs"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(386, 100)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(78, 15)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Customer ID:"
+        '
+        'txtCustomerID
+        '
+        Me.txtCustomerID.Location = New System.Drawing.Point(385, 122)
+        Me.txtCustomerID.Name = "txtCustomerID"
+        Me.txtCustomerID.Size = New System.Drawing.Size(82, 20)
+        Me.txtCustomerID.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(567, 171)
+        Me.Controls.Add(Me.txtCustomerID)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnBuy)
+        Me.Controls.Add(Me.lblTotals)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.btnRemove)
+        Me.Controls.Add(Me.lstCart)
+        Me.Controls.Add(Me.lstShelf)
+        Me.ForeColor = System.Drawing.SystemColors.MenuHighlight
+        Me.Name = "Form1"
+        Me.Text = "Shopping Cart"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lstShelf As ListBox
+    Friend WithEvents lstCart As ListBox
+    Friend WithEvents btnRemove As Button
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents lblTotals As Label
+    Friend WithEvents btnBuy As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtCustomerID As TextBox
+End Class
---
+++ Shopping Cart/Form1.vb
@@ -0,0 +1,112 @@
+﻿Public Class Form1
+    'Initializing some essential variables towards processing a cusomter's order
+    'A parallel array is also initialized alongside of the DVDs list box or lstShelf
+    Dim dblPrices = New Double() {10, 20, 30.99, 5.99, 5, 25.99, 29.99, 30.0, 20, 20}
+    Dim dblSubTotal As Double = 0
+    Dim intShipping As Integer = 0
+    Dim dblTaxRate As Double = 0.04
+    Dim Tax As Double
+    Dim dblTotal As Double
+    Dim strID As String
+
+    'Upon application startup this event reads all data within the listDVDs.txt file
+    'and puts it into lstShelf
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim fileIn As IO.StreamReader
+
+        If IO.File.Exists("listDVDs.txt") Then
+
+            fileIn = IO.File.OpenText("listDVDs.txt")
+            Do Until fileIn.Peek = -1
+                lstShelf.Items.Add(fileIn.ReadLine)
+            Loop
+            fileIn.Close()
+
+        End If
+
+    End Sub
+    'Clicking this button updates several variables depedning on the selected index from lstShelf
+    'the lstShelf index is then used on a parallel array to get the cost of the DVD
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+
+        Try
+            lstCart.Items.Add(lstShelf.SelectedItem)
+            dblSubTotal += dblPrices(lstShelf.SelectedIndex)
+
+            Tax = dblTaxRate * dblSubTotal
+
+            If (intShipping <> 5) Then
+                intShipping += 1
+            End If
+
+            dblTotal = dblSubTotal + Tax + intShipping
+
+            lblTotals.Text = "SubTotal: " & dblSubTotal.ToString("C2") & vbNewLine & "Tax: " & Tax.ToString("C2") & vbNewLine & "Shipping: " & intShipping.ToString("C2") & vbNewLine & Space(5) & "Total: " & dblTotal.ToString("C2")
+
+        Catch ex As Exception
+            lblTotals.Text = "ERROR NO ITEM SELECTED"
+        End Try
+
+    End Sub
+    'This click event activates, when a valid id is input, a StreamWriter object which then
+    'writes the subtotal, total, tax, and shipping variables into a text document called customerReceipts
+    Private Sub btnBuy_Click(sender As Object, e As EventArgs) Handles btnBuy.Click
+
+        Dim outFile As IO.StreamWriter
+
+        If txtCustomerID.Text.Length = 4 Then
+
+            strID = txtCustomerID.Text
+
+            outFile = IO.File.AppendText("customerReceipts.txt")
+
+            outFile.WriteLine(strID & Space(12) & dblTotal.ToString("C2") & Space(13 - dblTotal.ToString.Length) & dblSubTotal.ToString("C2") & Space(14 - dblSubTotal.ToString.Length) & Tax & Space(10 - Tax.ToString.Length) & intShipping)
+
+            outFile.Close()
+
+            dblSubTotal = 0
+            Tax = 0
+            dblTotal = 0
+            intShipping = 0
+            lblTotals.Text = ""
+            txtCustomerID.Text = ""
+            lstCart.Items.Clear()
+
+        Else
+
+            lblTotals.Text = "PLEASE ENTER CUSTOMER ID THAT IS 4 CHARACTERS LONG"
+
+        End If
+
+    End Sub
+
+    'Closes the program
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    'Does the opposite of the btnAdd click event by reversing the changes btnAdd made towards 
+    'the subtotal, tax,  shipping, and total variables
+    Private Sub btnRemove_Click(sender As Object, e As EventArgs) Handles btnRemove.Click
+        Try
+
+            dblSubTotal -= dblPrices(lstShelf.Items.IndexOf(lstCart.SelectedItem))
+            lstCart.Items.RemoveAt(lstCart.SelectedIndex)
+
+            Tax = dblTaxRate * dblSubTotal
+
+            If (intShipping <= 5 And intShipping > 0) Then
+                intShipping -= 1
+            End If
+
+            dblTotal = dblSubTotal + Tax + intShipping
+
+            lblTotals.Text = "SubTotal: " & dblSubTotal.ToString("C2") & vbNewLine & "Tax: " & Tax.ToString("C2") & vbNewLine & "Shipping: " & intShipping.ToString("C2") & vbNewLine & "       Total: " & dblTotal.ToString("C2")
+
+        Catch ex As Exception
+
+            lblTotals.Text = "ERROR NO ITEM SELECTED"
+
+        End Try
+    End Sub
+End Class
